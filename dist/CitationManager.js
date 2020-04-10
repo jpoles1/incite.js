f63ce7e89b57fe3a849e78e0030612c86162a60e
Jordan Poles <jpdev.noreply@gmail.com>
2020-04-10 16:51:31 -0400

    Release 1.1

--- dist/CitationManager.js
+++ dist/CitationManager.js
@@ -6,8 +6,9 @@ var CitationManager = /** @class */ (function () {
         this.refList = refList || [];
     }
     // Searches for existing citations (based on class), and attempts to replace them from list of citations"
-    CitationManager.prototype.augmentCitations = function () {
+    CitationManager.prototype.augmentCitations = function (onHover) {
         var _this = this;
+        if (onHover === void 0) { onHover = true; }
         var refIDList = this.refList.map(function (c) { return c.refID; });
         var inactiveCites = document.querySelectorAll(".inline-cite:not(.activated)");
         Array.from(inactiveCites).forEach(function (citeElem) {
@@ -16,7 +17,11 @@ var CitationManager = /** @class */ (function () {
             var refIndex = refIDList.indexOf(refID);
             if (refIndex !== -1) {
                 var cite = _this.refList[refIndex];
-                citeElem.innerHTML = "\n\t\t\t\t\t<span hidden class=\"rawRef\">" + rawRef + "</span>\n\t\t\t\t\t<sup onclick=\"this.nextSibling.nextElementSibling.classList.toggle('inline-cite-popup-show'); setTimeout(() => {this.nextSibling.nextElementSibling.classList.remove('inline-cite-popup-show')}, 20000)\">[ref]</sup>\n\t\t\t\t\t<div class=\"inline-cite-popup\">\n\t\t\t\t\t\t" + cite.genInlineCite() + "\n\t\t\t\t\t</div>\n\t\t\t\t";
+                var eventHandler = "onclick=\"this.nextSibling.nextElementSibling.classList.toggle('inline-cite-popup-show')\"";
+                if (onHover) {
+                    eventHandler = "onmouseover=\"this.nextSibling.nextElementSibling.classList.add('inline-cite-popup-show')\"\n\t\t\t\t\tonmouseout=\"this.nextSibling.nextElementSibling.classList.remove('inline-cite-popup-show')\"";
+                }
+                citeElem.innerHTML = "\n\t\t\t\t\t<span hidden class=\"rawRef\">" + rawRef + "</span>\n\t\t\t\t\t<sup " + eventHandler + ">\n\t\t\t\t\t\t[ref]\n\t\t\t\t\t</sup>\n\t\t\t\t\t<div class=\"inline-cite-popup\">\n\t\t\t\t\t\t" + cite.genInlineCite() + "\n\t\t\t\t\t</div>\n\t\t\t\t";
             }
         });
     };
