29d7f73522b6773480be49831e8fa9a084186de2
Jordan Poles <jpdev.noreply@gmail.com>
2020-04-17 15:52:32 -0400

    Release 1.3

--- dist/CitationManager.js
+++ dist/CitationManager.js
@@ -10,23 +10,33 @@ var CitationManager = /** @class */ (function () {
         var _this = this;
         if (onHover === void 0) { onHover = true; }
         var refIDList = this.refList.map(function (c) { return c.refID; });
-        var inactiveCites = document.querySelectorAll(".inline-cite:not(.activated)");
+        var inactiveCites = document.querySelectorAll(".incite:not(.activated)");
         Array.from(inactiveCites).forEach(function (citeElem) {
             var rawRef = citeElem.innerHTML;
-            var refID = rawRef.replace(/[\[\]]/g, "");
-            var refIndex = refIDList.indexOf(refID);
-            if (refIndex !== -1) {
-                var cite = _this.refList[refIndex];
-                var short = "[" + (refIndex + 1) + "]";
-                var eventHandler = "onclick=\"this.nextSibling.nextElementSibling.classList.toggle('inline-cite-popup-show')\"";
-                if (onHover) {
-                    eventHandler = "onmouseover=\"this.nextSibling.nextElementSibling.classList.add('inline-cite-popup-show')\"\n\t\t\t\t\tonmouseout=\"this.nextSibling.nextElementSibling.classList.remove('inline-cite-popup-show')\"";
+            var refList = rawRef.replace(/[\[\]]/g, "").split("|");
+            var shortList = [];
+            var citeNameList = [];
+            refList.forEach(function (refID) {
+                var refIndex = refIDList.indexOf(refID);
+                if (refIndex !== -1) {
+                    var cite = _this.refList[refIndex];
+                    var short = "" + (refIndex + 1);
+                    var citeName = cite.name;
                     if (cite.url) {
-                        short = "<a href=\"" + cite.url + "\" target=\"_blank\">" + short + "</a>";
+                        citeName = "<a href=\"" + cite.url + "\" target=\"_blank\">" + cite.name + "</a>";
+                        if (onHover) {
+                            short = "<a href=\"" + cite.url + "\" target=\"_blank\">" + short + "</a>";
+                        }
                     }
+                    shortList.push(short);
+                    citeNameList.push(refIndex + 1 + ") " + citeName);
                 }
-                citeElem.innerHTML = "\n\t\t\t\t\t<span hidden class=\"rawRef\">" + rawRef + "</span>\n\t\t\t\t\t<sup " + eventHandler + ">\n\t\t\t\t\t\t" + short + "\n\t\t\t\t\t</sup>\n\t\t\t\t\t<div class=\"inline-cite-popup\">\n\t\t\t\t\t\t" + cite.genInlineCite() + "\n\t\t\t\t\t</div>\n\t\t\t\t";
+            });
+            var eventHandler = "onclick=\"this.nextSibling.nextElementSibling.classList.toggle('incite-popup-show')\"";
+            if (onHover) {
+                eventHandler = "onmouseover=\"this.nextSibling.nextElementSibling.classList.add('incite-popup-show')\"\n\t\t\t\tonmouseout=\"this.nextSibling.nextElementSibling.classList.remove('incite-popup-show')\"";
             }
+            citeElem.innerHTML = "\n\t\t\t\t\t<span hidden class=\"rawRef\">" + rawRef + "</span>\n\t\t\t\t\t<sup " + eventHandler + ">\n\t\t\t\t\t\t[" + shortList.join(", ") + "]\n\t\t\t\t\t</sup>\n\t\t\t\t\t<div class=\"incite-popup\">\n\t\t\t\t\t\t" + citeNameList.join("<hr>") + "\n\t\t\t\t\t</div>\n\t\t\t\t";
         });
     };
     return CitationManager;
